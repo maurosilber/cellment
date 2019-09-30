048099c7f069de79db319b8a8b33911c5ad68949
Mauro Silberberg <maurosilber@gmail.com>
2019-09-30 17:31:11 -0300

    Updated tracking module documentation and setup.py
    Signed-off-by: Mauro Silberberg <maurosilber@gmail.com>

--- cellment/tracking.py
+++ cellment/tracking.py
@@ -6,9 +6,12 @@ from skimage.measure import regionprops
 def labels_graph(labels_stack) -> nx.DiGraph:
     """Returns a time-like DAG of connected labels.
 
+    Node names are (time, label).
+
     Parameters
     ----------
     labels_stack : numpy.array
+        Axis 0 corresponds to the temporal dimension.
 
     Returns
     -------
@@ -19,40 +22,51 @@ def labels_graph(labels_stack) -> nx.DiGraph:
     # Nodes
     for t, labels in enumerate(labels_stack):
         for prop in regionprops(labels):
-            if prop.label == 0:
+            if prop.label == 0:  # Exclude background
                 continue
             node_props = {'area': prop.area}
             graph.add_node((t, prop.label), **node_props)
 
     # Edges
-    for t in range(len(labels_stack) - 1):
-        prev_labels, labels = labels_stack[t:][:2]
+    for t, labels in enumerate(labels_stack[1:], 1):
+        prev_labels = labels_stack[t - 1]
         for i in np.unique(prev_labels):
-            if i > 0:
-                intersection = labels[prev_labels == i]
-                for j in np.unique(intersection):
-                    if j > 0:
-                        edge_props = {'area': (intersection == j).sum()}
-                        graph.add_edge((t, i), (t + 1, j), **edge_props)
+            if i == 0:  # Exclude background
+                continue
+            intersection = labels[prev_labels == i]
+            for j in np.unique(intersection):
+                if j == 0:  # Exclude background
+                    continue
+                edge_props = {'area': (intersection == j).sum()}
+                graph.add_edge((t - 1, i), (t, j), **edge_props)
 
     return graph
 
 
 def decompose(graph):
+    """Decomposes a graph into disconnected components."""
     cc = nx.connected_components(graph.to_undirected(as_view=True))
     return (graph.subgraph(c) for c in cc)
 
 
 def time_indices(graph):
+    """Returns all time indexes."""
     return (t for t, n in graph.nodes)
 
 
 def nodes_at_time(graph, time):
+    """Returns all nodes existing at the given time index."""
     return ((t, n) for t, n in graph.nodes if t == time)
 
 
-def get_chains(graph):
-    count = np.bincount(list(time_indices(graph)))
+def get_timelike_chains(graph):
+    """Returns timelike chains from graph.
+
+    Only returns timelike chains where the number of coexisting labels,
+    that is, having the same time index, equals the maximum number of
+    coexisting labels.
+    """
+    count = np.bincount(list(time_indices(graph)))  # Number of coexisting labels
     times_most_elements = np.argwhere(count == count.max())[:, 0]
     time_chains = np.split(times_most_elements,
                            1 + np.argwhere(np.diff(times_most_elements) > 1)[:, 0])
@@ -71,5 +85,6 @@ def get_chains(graph):
     return subgraphs
 
 
-def is_chain(graph):
+def is_timelike_chain(graph):
+    """Returns True if the graph has only one node per time."""
     return len(set(time_indices(graph))) == len(graph)
