624698f978cb2cc3f3a21ebf1322e0e24cb0b1ca
Mauro Silberberg <maurosilber@gmail.com>
2019-11-15 16:47:36 -0300

    Update version number
    Signed-off-by: Mauro Silberberg <maurosilber@gmail.com>

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='cellment',
-    version='0.0.2',
+    version='0.1.0',
     packages=['cellment'],
     url='https://github.com/maurosilber/cellment',
     license='MIT',
     author='Mauro Silberberg',
     author_email='maurosilber@gmail.com',
     description='Segmentation of cells from fluorescence microscopy.',
-    install_requires=['numpy', 'scipy', 'networkx', 'scikit-image']
+    install_requires=['numpy', 'scipy', 'networkx', 'scikit-image', 'more_itertools']
 )
