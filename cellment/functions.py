21c9be0e11fec36d4ff77ab84d3c17f4417e8d60
Mauro Silberberg <maurosilber@gmail.com>
2020-01-10 14:49:42 -0300

    Updated functions module and added tests.
    Signed-off-by: Mauro Silberberg <maurosilber@gmail.com>

--- cellment/functions.py
+++ cellment/functions.py
@@ -1,89 +1,93 @@
 import numpy as np
 from scipy import ndimage, stats
+from scipy.ndimage._ni_support import _normalize_sequence
 
 
-def normalized_vector(vector):
+def normalized_vector(vector, axis=-1):
     """Normalizes a vector field.
 
     Parameters
     ----------
     vector : numpy.array
-        The input vector field (ndim, ...).
+        Vector field.
+    axis : int
+        Axis of vector coordinates. By default, last axis.
 
     Returns
     -------
     numpy.array
-        Normalized gradient.
+        Normalized vector field.
 
     """
-    norm = np.linalg.norm(vector, axis=0)
-    cond = norm > 0
-    vector[:, cond] = vector[:, cond] / norm[cond]
-    return vector
+    norm = np.linalg.norm(vector, axis=axis, keepdims=True)
+    return np.divide(vector, norm, where=(norm > 0))
 
 
-def normalized_gradient(image):
-    """Calculates the normalized gradient of an image.
-
-    Parameters
-    ----------
-    image : numpy.array
-        Input image.
-
-    Returns
-    -------
-    numpy.array
-        The normalized gradient of the image.
-    """
-    grad = np.array(np.gradient(image.astype(float)))
-    grad = normalized_vector(grad)
-    return grad
-
-
-def silver_mountain_operator(input, sigma, size):
-    """Applies the Silver Mountain Operator (SMO) to the input.
+def normalized_gradient(input):
+    """Calculates the normalized gradient of a scalar field.
 
     Parameters
     ----------
     input : numpy.array
+        Input field.
+
+    Returns
+    -------
+    numpy.array
+        The normalized gradient of the scalar field.
+    """
+    grad = np.array(np.gradient(input.astype(float, copy=False)), ndmin=2)
+    return normalized_vector(grad, axis=0)
+
+
+def silver_mountain_operator(input, sigma, size):
+    """Applies the Silver Mountain Operator (SMO) to a scalar field.
+
+    Parameters
+    ----------
+    input : numpy.array
+        Input field.
     sigma : scalar or sequence of scalars
-        Standard deviation for Gaussian kernel. The standard
-        deviations of the Gaussian filter are given for each axis as a
-        sequence, or as a single number, in which case it is equal for
-        all axes.
-    size : int or tuple of int
+        Standard deviation for Gaussian kernel.
+    size : int or sequence of int
         Averaging window size.
 
     Returns
     -------
     numpy.array
     """
-    size = ndimage._ni_support._normalize_sequence(size, input.ndim)
-    input = ndimage.gaussian_filter(input.astype(float), sigma=sigma)
+    size = _normalize_sequence(size, input.ndim)
+    input = ndimage.gaussian_filter(input.astype(float, copy=False), sigma=sigma)
     norm_grad = normalized_gradient(input)
     sliding_mean = ndimage.uniform_filter(norm_grad, size=(1, *size))
-    mod = np.linalg.norm(sliding_mean, axis=0)
-    return mod
+    magnitude = np.linalg.norm(sliding_mean, axis=0)
+    return magnitude
 
 
 class HistogramRV(stats.rv_histogram):
-    """Returns a random variable from an array of observations.
-
-    data : numpy.array
-        Array of observations. If it is multidimensional, it will be flattened.
-    bins : int or str
-        See numpy.histogram.
-    """
-
     @classmethod
     def from_data(cls, data, bins='fd'):
-        return cls(np.histogram(data, bins=bins))
+        """Generates RV from data.
+
+        data : array-like
+            Array of observations. If it is multidimensional, it will be flattened.
+        """
+        if np.issubdtype(data.dtype, np.integer):
+            data = np.ma.asarray(data)
+            hist = np.bincount(data.compressed())
+            bins = np.arange(hist.size + 1)
+            # TODO: Keep only non-zero entries.
+            return cls((hist, bins))
+        else:
+            return cls(np.histogram(data, bins=bins))
 
     def save(self, path):
+        """Saves histogram to an npz file."""
         hist, bins = self._histogram
         np.savez(path, hist=hist, bins=bins)
 
     @classmethod
     def load(cls, path):
+        """Loads histogram from npz file."""
         file = np.load(path)
         return cls((file['hist'], file['bins']))
---
+++ cellment/tests/functions.py
@@ -0,0 +1,43 @@
+import unittest
+
+import numpy as np
+
+from ..functions import normalized_vector, normalized_gradient
+
+
+class NormalizationTest(unittest.TestCase):
+    def test_normalized_vector(self):
+        n = np.array([[0, 0],
+                      [1, 0],
+                      [0, 1],
+                      [1 / np.sqrt(2), 1 / np.sqrt(2)]])
+        self.assertTrue(np.allclose(normalized_vector(n), n))
+
+    def test_non_normalized_vector(self):
+        n = 2 * np.array([[0, 0],
+                          [1, 0],
+                          [0, 1],
+                          [1 / np.sqrt(2), 1 / np.sqrt(2)]])
+        self.assertTrue(np.allclose(normalized_vector(n), n / 2))
+
+    def test_axis(self):
+        n = np.array([[1, 0],
+                      [1, 0]])
+        self.assertTrue(np.allclose(normalized_vector(n, axis=1), n))
+        self.assertTrue(np.allclose(normalized_vector(n, axis=0), n / np.sqrt(2)))
+
+
+class GradientTest(unittest.TestCase):
+    def test_zero_gradient(self):
+        x = np.zeros(5)
+        self.assertTrue(np.allclose(normalized_gradient(x), x))
+
+    def test_gradient(self):
+        self.assertTrue(np.allclose(normalized_gradient(np.arange(5)),
+                                    np.ones(5)))
+        self.assertTrue(np.allclose(normalized_gradient(2 * np.arange(5)),
+                                    np.ones(5)))
+
+
+if __name__ == '__main__':
+    unittest.main()
