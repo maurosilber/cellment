81f7ca2ce5565bfc4acfe5f8374e1d3c328fc480
Mauro Silberberg <maurosilber@gmail.com>
2020-04-21 12:56:58 -0300

    Changed color_mapping to handle a stack of labels
    Color_mapping function in plotting module now receives as input a labeled images stack.
    
    Signed-off-by: Mauro Silberberg <maurosilber@gmail.com>

--- cellment/plotting.py
+++ cellment/plotting.py
@@ -4,20 +4,21 @@ from skimage import morphology
 from skimage.filters import gaussian
 
 
-def color_mapping(labeled_image):
-    mask = (labeled_image == 0)  # Background mask
-
-    # Expand labels to cover background area
-    watershed = gaussian((~mask).astype(float), 10)
-    watershed = morphology.watershed(-watershed, labeled_image)
-
-    # Find connected labels
+def color_mapping(labeled_images):
     graph = nx.Graph()
-    for label in np.unique(watershed):
-        mask = (watershed == label)
-        mask = morphology.binary_dilation(mask, morphology.square(2))  # Expand mask
-        for connected_label in np.unique(watershed[mask]):
-            graph.add_edge(label, connected_label)
+    for labeled_image in labeled_images:
+        mask = (labeled_image == 0)  # Background mask
+
+        # Expand labels to cover background area
+        watershed = gaussian((~mask).astype(float), 10)
+        watershed = morphology.watershed(-watershed, labeled_image)
+
+        # Find connected labels
+        for label in np.unique(watershed):
+            mask = (watershed == label)
+            mask = morphology.binary_dilation(mask, morphology.square(2))  # Expand mask
+            for connected_label in np.unique(watershed[mask]):
+                graph.add_edge(label, connected_label)
 
     return nx.coloring.greedy_color(graph)
 
