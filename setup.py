a3cfc41bb4d76a86c9bbee0d62ee25aaa6b25f5c
Mauro Silberberg <maurosilber@gmail.com>
2020-06-02 16:23:36 -0300

    Modern Python packaging

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
 from setuptools import setup
 
-setup(
-    name="cellment",
-    version="0.1.0",
-    packages=["cellment"],
-    url="https://github.com/maurosilber/cellment",
-    license="MIT",
-    author="Mauro Silberberg",
-    author_email="maurosilber@gmail.com",
-    description="Segmentation of cells from fluorescence microscopy.",
-    install_requires=["numpy", "scipy", "networkx", "scikit-image", "more_itertools"],
-)
+if __name__ == "__main__":
+    setup()
